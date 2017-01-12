47944ac7b8a1f19b5c1dac415648d07062e78290
Matija Nalis <mnalis-rpi@voyager.hr>
2017-01-12 18:38:46 +0100

    also log with TEMPer USB thermometer (from https://github.com/mnalis/TEMPer-DX)

--- temp_fetch.py
+++ temp_fetch.py
@@ -2,13 +2,19 @@ from subprocess import check_output
 from re import findall
 from time import sleep, strftime, time
 
-def get_temp():
+def get_temp_rpi_internal():
     temp = check_output(["vcgencmd","measure_temp"]).decode("UTF-8")
+    temp = float(findall("\d+\.\d+",temp)[0])
+    return(temp)
+
+def get_temp_temper_usb():
+    temp = check_output(["/usr/local/bin/temper"]).decode("UTF-8")
     temp = float(findall("\d+\.\d+",temp)[0])
     return(temp)
 
 with open("cpu_temp.csv", "a") as log:
     while True:
-        temp = get_temp()
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+        temp1 = get_temp_rpi_internal()
+        temp2 = get_temp_temper_usb()
+        log.write ("{0},{1},{2}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp1),str(temp2)))
         sleep(1)
