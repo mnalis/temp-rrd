4f8aa55a303fe6b2a45a5b3f84914c299264321e
Matija Nalis <mnalis-rpi@voyager.hr>
2017-01-15 02:44:42 +0100

    verbosity params

--- temp_fetch.py
+++ temp_fetch.py
@@ -11,22 +11,31 @@ databaseFile = "/run/temperature_log/temperature_log.rrd"
 def get_temp_rpi_internal():
     temp = check_output(["vcgencmd","measure_temp"]).decode("UTF-8")
     temp = float(findall("\d+\.\d+",temp)[0])
+    if args.verbose:
+        print "rPi internal temperature is %s" % temp
     return(temp)
 
 def get_temp_temper_usb():
     temp = check_output(["/usr/local/bin/temper"]).decode("UTF-8")
     temp = float(findall("\d+\.\d+",temp)[0])
+    if args.verbose:
+        print "USB TEMPer temperature is %s" % temp
     return(temp)
 
 def get_temp_outside():
     import urllib2
     import json
-    f = urllib2.urlopen('http://api.wunderground.com/api/' + args.apikey + '/geolookup/conditions/q/pws:' + args.apiid + '.json')
+
+    if not args.apiid:
+        args.apiid = 'autoip'
+
+    f = urllib2.urlopen('http://api.wunderground.com/api/' + args.apikey + '/geolookup/conditions/q/' + args.apiid + '.json')
     json_string = f.read()
     parsed_json = json.loads(json_string)
-    #location = parsed_json['location']['city']
     temp = parsed_json['current_observation']['temp_c']
-    #print "Current temperature in %s is: %s" % (location, temp_c)
+    if args.verbose:
+        location = parsed_json['location']['city']
+        print "Current outside temperature in %s (specified %s) is: %s" % (location, args.apiid, temp)
     f.close()
     return(temp)
 
@@ -46,12 +55,14 @@ def update_all():
 
     update = update[:-1]
     template = template[:-1]
-    #print "DEBUG: rrdtool update %s --template %s %s" % (databaseFile, template, update)
+    if args.verbose > 1:
+        print "DEBUG: rrdtool update %s --template %s %s" % (databaseFile, template, update)
     rrdtool.update(databaseFile, "--template", template, update)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--apikey", help="wunderground.com API key, register to get one")
 parser.add_argument("--apiid", help="wunderground.com location ID, like IGRADZAG10")
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="count")
 args = parser.parse_args()
 
 update_all()
