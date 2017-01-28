b9a559922f1c397c0075e2d07c38f0e98eb7c562
Matija Nalis <mnalis-rpi@voyager.hr>
2017-01-28 17:58:46 +0100

    add week/month/year graphs (to ramdisk)

--- graph.sh
+++ graph.sh
@@ -1,8 +1,55 @@
 #!/bin/sh
 
-rrdtool graph /var/www/html/temperature/one_day.png \
-  --title "Temperature (degrees C), 1d" \
+exec > /dev/null
+
+rrdtool graph  /run/temperature_log/one_day.png \
+  --title "Temperature (degrees C), 1 day" \
   --start now-1d --end now \
+  --width=640 --height=480 \
+  --lower-limit=-10 \
+  --step=60 -v degreesC \
+  DEF:temp1=/run/temperature_log/temperature_log.rrd:rpi:AVERAGE \
+  DEF:temp2=/run/temperature_log/temperature_log.rrd:usbtemper:AVERAGE \
+  DEF:temp3=/run/temperature_log/temperature_log.rrd:outside:AVERAGE \
+  LINE2:temp1#A00000:"rPi internal" \
+  LINE2:temp2#008000:"USB TEMPer" \
+  AREA:temp3#00008080:"Outside" \
+  HRULE:0#0000FF:"freezing" \
+  HRULE:18#00FFFF:"cold"
+
+rrdtool graph  /run/temperature_log/one_week.png \
+  --title "Temperature (degrees C), 1 week" \
+  --start now-7d --end now \
+  --width=640 --height=480 \
+  --lower-limit=-10 \
+  --step=60 -v degreesC \
+  DEF:temp1=/run/temperature_log/temperature_log.rrd:rpi:AVERAGE \
+  DEF:temp2=/run/temperature_log/temperature_log.rrd:usbtemper:AVERAGE \
+  DEF:temp3=/run/temperature_log/temperature_log.rrd:outside:AVERAGE \
+  LINE2:temp1#A00000:"rPi internal" \
+  LINE2:temp2#008000:"USB TEMPer" \
+  AREA:temp3#00008080:"Outside" \
+  HRULE:0#0000FF:"freezing" \
+  HRULE:18#00FFFF:"cold"
+
+rrdtool graph  /run/temperature_log/one_month.png \
+  --title "Temperature (degrees C), 1 month" \
+  --start now-30d --end now \
+  --width=640 --height=480 \
+  --lower-limit=-10 \
+  --step=60 -v degreesC \
+  DEF:temp1=/run/temperature_log/temperature_log.rrd:rpi:AVERAGE \
+  DEF:temp2=/run/temperature_log/temperature_log.rrd:usbtemper:AVERAGE \
+  DEF:temp3=/run/temperature_log/temperature_log.rrd:outside:AVERAGE \
+  LINE2:temp1#A00000:"rPi internal" \
+  LINE2:temp2#008000:"USB TEMPer" \
+  AREA:temp3#00008080:"Outside" \
+  HRULE:0#0000FF:"freezing" \
+  HRULE:18#00FFFF:"cold"
+
+rrdtool graph  /run/temperature_log/one_year.png \
+  --title "Temperature (degrees C), 1 year" \
+  --start now-365d --end now \
   --width=640 --height=480 \
   --lower-limit=-10 \
   --step=60 -v degreesC \
