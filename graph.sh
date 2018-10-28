b82e4af4d6062e2c54046e90420f769e1b397734
Matija Nalis <mnalis-rpi@voyager.hr>
2018-10-29 00:08:32 +0100

    remove spaces to temporarily fix $COMMON params

--- graph.sh
+++ graph.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-exec > /dev/null
+#exec > /dev/null
 
-COMMON=<<EOF
-  --width=640 --height=480 \
-  --lower-limit=-10 \
-  --step=60 -v degreesC \
-  DEF:temp1=/run/temperature_log/temperature_log.rrd:rpi:AVERAGE \
-  DEF:temp2=/run/temperature_log/temperature_log.rrd:usbtemper:AVERAGE \
-  DEF:temp3=/run/temperature_log/temperature_log.rrd:outside:AVERAGE \
-  LINE2:temp1#A00000:"rPi internal" \
-  LINE2:temp2#008000:"USB TEMPer" \
-  AREA:temp3#00008080:"Outside" \
-  HRULE:0#0000FF:"freezing" \
+COMMON='
+  --width=640 --height=480 
+  --lower-limit=-10 
+  --step=60 -v degreesC 
+  DEF:temp1=/run/temperature_log/temperature_log.rrd:rpi:AVERAGE 
+  DEF:temp2=/run/temperature_log/temperature_log.rrd:usbtemper:AVERAGE 
+  DEF:temp3=/run/temperature_log/temperature_log.rrd:outside:AVERAGE 
+  LINE2:temp1#A00000:"rPi_internal" 
+  LINE2:temp2#008000:"USB_TEMPer" 
+  AREA:temp3#00008080:"Outside" 
+  HRULE:0#0000FF:"freezing" 
   HRULE:18#00FFFF:"cold"
-EOF
+'
 
 rrdtool graph  /run/temperature_log/one_day.png \
   --title "Temperature (degrees C), 1 day" \
