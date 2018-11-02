f1cd5c720b495ed98030474e5cced2bcb8457ba7
Matija Nalis <mnalis-rpi@voyager.hr>
2018-11-02 18:06:20 +0100

    move outside temp fetching to separate scripte outside_temp

--- temp_fetch.py
+++ temp_fetch.py
@@ -30,22 +30,11 @@ def get_temp_temper_usb():
     return(temp)
 
 def get_temp_outside():
-    import urllib2
-    import json
-
-    try:
-        f = urllib2.urlopen('https://api.openweathermap.org/data/2.5/weather?APPID=' + args.apikey + '&units=metric&' + args.apiloc)
-        json_string = f.read()
-        parsed_json = json.loads(json_string)
-        temp = parsed_json['main']['temp']
-        if args.verbose:
-            location = parsed_json['name']
-            print "Current outside temperature at %s (specified %s) is: %s" % (location, args.apiloc, temp)
-        f.close()
-        return(temp)
-    except:
-        return
-
+    temp = check_output(["/usr/local/bin/outside_temp"]).decode("UTF-8")
+    temp = float(findall("\d+\.?\d*",temp)[0])
+    if args.verbose:
+        print "Outside temperature is %s" % temp
+    return(temp)
 
 def update_all():
     template = ""
@@ -63,11 +52,10 @@ def update_all():
     template += "dht11hum:"
     update += "%f:" % float(dht11_h)
 
-    if args.apikey:
-        out_temp = get_temp_outside()
-        if out_temp:
-            template += "outside:"
-            update += "%f:" % out_temp
+    out_temp = get_temp_outside()
+    if out_temp:
+        template += "outside:"
+        update += "%f:" % out_temp
 
     update = update[:-1]
     template = template[:-1]
@@ -76,8 +64,6 @@ def update_all():
     rrdtool.update(databaseFile, "--template", template, update)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--apikey", help="openweathermap.org API key, register to get one")
-parser.add_argument("--apiloc", help="openweathermap.org locator from https://openweathermap.org/current, like 'q=London,uk' or 'lat=45.80303&lon=15.92889'")
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="count")
 args = parser.parse_args()
 
